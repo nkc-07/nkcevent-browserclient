b72dd32a5f0a39d2e08efbc1dae2f1e2a8ee8e3a
tsukasaG273 <ty1790@mailg.denpa.ac.jp>
2020-07-02 11:56:37 +0900

    login.jsの作成確認願う

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@
  */
 
 //ログイン処理
-function Login(){
+function getLogin(){
 
   var userID = "test"
   //var userId = getField("user_id");
@@ -16,15 +16,42 @@ function Login(){
   if (userId.value.length > 0 && password.value.length > 0) {
         disabled = false;
     }
-
     //var login = getField("login");
 
     if (disabled) {
-        console.log("ログイン成功")
+      console.log("ログイン成功")
+      $.ajax({
+        url: './api/member/', //送信先
+        type: 'GET', //送信方法
+        datatype: 'json', //受け取りデータの種類
+        data: {
+        }})
+        .done(function(response) {
+            console.log('通信成功PostMembrparticipation');
+            console.log(response);
+        })
+        .fail(function(response) {
+            console.log('通信失敗');
+            console.log(response);
+        })
     }
  　　else {
  　　　console.log("ログイン失敗")
+ $.ajax({
+    url: './api/member/', //送信先
+    type: 'GET', //送信方法
+    datatype: 'json', //受け取りデータの種類
+    data: {
+    }
+})
+.done(function(response) {
+    console.log('通信成功PostMembrparticipation');
+    console.log(response);
+})
+.fail(function(response) {
+    console.log('通信失敗');
+    console.log(response);
+})
 　  }
-
-
 }
+
