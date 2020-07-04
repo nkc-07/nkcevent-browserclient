74ffc1ed8e7a7194a6e88d7c17536ac7197341d0
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-07-04 16:42:03 +0900

    apiパスを追加

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,40 @@
+// ログイン処理
 $(function() {
     $('input').on('keydown', function(e) {
         if (e.which == 13) {
-            console.log($('#exampleInputEmail1').val())
-            console.log($('#exampleInputPassword1').val())
+            getLogin(
+                $('#exampleInputEmail1').val(),
+                $('#exampleInputPassword1').val()
+            )
         }
     })
     $('.login-button').on('click', function() {
-        console.log($('#exampleInputEmail1').val())
-        console.log($('#exampleInputPassword1').val())
+        getLogin(
+            $('#exampleInputEmail1').val(),
+            $('#exampleInputPassword1').val()
+        )
     });
 });
+
+
+//ログイン処理
+function getLogin(userID, password) {
+    $.ajax({
+            url: '../../api/member/login.php', //送信先
+            type: 'GET', //送信方法
+            datatype: 'json', //受け取りデータの種類
+            data: {
+                'mailaddress': userID,
+                'password': password,
+            }
+        })
+        .done(function(response) {
+            console.log('通信成功PostMembrparticipation');
+            console.log(response);
+            if (response) { console.log('ログイン成功') }
+        })
+        .fail(function(response) {
+            console.log('通信失敗');
+            console.log(response);
+        })
+}
