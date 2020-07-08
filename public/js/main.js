4a73837ef7d994ce9f43f857eaee551fbf8382ef
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-07-08 10:46:48 +0900

    ログインの成功処理を追加

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
-let userId;
-let password;
+let loginInfo = {
+    userId: undefined,
+    password: undefined
+};
 
-// ログイン処理
+// formのイベント
 $(function() {
     $('input').on('keydown', function(e) {
         if (e.which == 13) {
@@ -13,43 +15,38 @@ $(function() {
     });
 });
 
-//ログイン処理
-function getLoginInfo(userID, password) {
+// ログイン処理
+function getLogin() {
+    $('.login-button').prop('disabled', true);
+    $('.login-err').show();
+    $('.login-button .login-text').hide();
+
+    loginInfo['userId'] = $('#exampleInputEmail1').val()
+    loginInfo['password'] = $('#exampleInputPassword1').val()
+
     $.ajax({
             url: '../../api/member/login.php', //送信先
             type: 'GET', //送信方法
             datatype: 'json', //受け取りデータの種類
             data: {
-                'mailaddress': userID,
-                'password': password,
+                'mailaddress': loginInfo['userId'],
+                'password': loginInfo['password'],
             }
         })
         .done(function(response) {
             console.log(response);
             if (response['data'] == 1) {
-                console.log('ログイン成功');
+                localStorage.setItem('logininfo', JSON.stringify(loginInfo));
+                window.location.href = "./mypage/";
             } else {
                 $('.login-button').prop('disabled', false);
                 $('.loading-icon').hide();
                 $('.login-button .login-text').show();
+                $('.login-err-text').show();
             }
         })
         .fail(function(response) {
             console.log('通信失敗');
             console.log(response);
         })
-}
-
-function getLogin() {
-    $('.login-button').prop('disabled', true);
-    $('.login-err').show();
-    $('.login-button .login-text').hide();
-
-    userId = $('#exampleInputEmail1').val()
-    password = $('#exampleInputPassword1').val()
-
-    getLoginInfo(
-        userId,
-        password
-    )
 }
