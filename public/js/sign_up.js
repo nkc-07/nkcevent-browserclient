a354f7342cc9b4a403c1a5da28fe6b33c383ebf0
masayuki kasano <mk1734@mailg.denpa.ac.jp>
2020-07-14 12:12:15 +0900

    エラーの表示、非表示の実装

--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -1,6 +1,33 @@
 $(function () {
   // Ajax button click
+
   $('#sign-up').on('click', function () {
+    if (!$('input[name=mall]').val() || $('input[name=mall]').val() == '') {
+      $('#mall-err').css('display', 'block')
+    } else {
+      $('#mall-err').css('display', 'none')
+    }
+
+    if (!$('input[name=pass]').val() || $('input[name=pass]').val() == '') {
+      $('#pass-err').css('display', 'block')
+    } else {
+      $('#pass-err').css('display', 'none')
+    }
+
+    if (!$('input[name=name]').val() || $('input[name=name]').val() == '') {
+      $('#name-err').css('display', 'block')
+    } else {
+      $('#name-err').css('display', 'none')
+    }
+
+    if (
+      !$('[name=gender]:checked').val() ||
+      $('[name=gender]:checked').val() == ''
+    ) {
+      $('#gender-err').css('display', 'block')
+    } else {
+      $('#gender-err').css('display', 'none')
+    }
     $.ajax({
       url: '../../api/member/memberinfo.php',
       type: 'POST',
@@ -17,6 +44,7 @@ $(function () {
           $('[name=day]').val()
       }
     })
+
       // Ajaxリクエストが成功した時発動
       .done(data => {
         $('.result').html(data)
@@ -26,20 +54,13 @@ $(function () {
       // Ajaxリクエストが失敗した時発動
       .fail(data => {
         $('.result').html(data)
-        console.log(data)
-        if (!document.getElementById('mall').value) {
-          $('mall-err-text').show()
-        }
+        $('#sign-up')
+          .on('click', function () {
+            console.log(data)
+          })
+          // Ajaxリクエストが成功・失敗どちらでも発動
 
-        if (!document.getElementById('pass').value) {
-          $('pass-err-text').show()
-        }
-
-        if (!document.getElementById('name').value) {
-          $('name-err-text').show()
-        }
+          .always(data => {})
       })
-      // Ajaxリクエストが成功・失敗どちらでも発動
-      .always(data => {})
   })
 })
