7b6c71b6a0e9365ae016a8c0092e8e0761a6b551
Hagiwara0305 <shionn35@gmail.com>
2020-07-19 19:48:27 +0900

    画面の修正

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,1 +1,18 @@
-console.log("success");
+$.ajax({
+        url: '/api/member/memberinfo.php', //送信先
+        type: 'GET', //送信方法
+        datatype: 'json', //受け取りデータの種類
+        data: {
+            token: localStorage.getItem('token')
+        }
+    })
+    .done(function(response) {
+        let memberInfo = response['data']['info']
+        let memberTag = response['data']['tag']
+
+
+
+        console.log(memberInfo)
+        console.log(memberTag)
+    })
+    .fail(function(response) {})
