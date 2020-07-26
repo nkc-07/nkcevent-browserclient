7b5c795834d6d7c0fb28f6eecdf7ef14f1000734
Hagiwara0305 <shionn35@gmail.com>
2020-07-26 14:52:33 +0900

    userを表示する処理を追加

--- public/js/new.js
+++ public/js/new.js
@@ -1,3 +1,17 @@
+$.ajax({
+    url: '/api/member/memberinfo.php', //送信先
+    type: 'GET', //送信方法
+    datatype: 'json', //受け取りデータの種類
+    data: {
+        token: localStorage.getItem('token')
+    }
+}).done(function(response) {
+    let memberInfo = response['data']['info'];
+
+    $('.user-img').attr('src', memberInfo['icon']);
+    $('.nickname').text(memberInfo['nickname']);
+});
+
 $('.input-day').change(function(e) {
     let date = ($(this).val()).split('-');
 
