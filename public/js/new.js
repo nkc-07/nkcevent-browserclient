dcad58bd3954c5f671b153e52e2a66dd7855715e
Hagiwara0305 <shionn35@gmail.com>
2020-07-26 14:40:28 +0900

    event-imageの画像を設定した時の表示処理を追加

--- public/js/new.js
+++ public/js/new.js
@@ -11,3 +11,11 @@ $('.create-day').text(
     toDay.getMonth() + '/' +
     toDay.getDate()
 );
+
+$('.send-event-img').on('change', function(e) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        $(".event-img").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
