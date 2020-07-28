38a9b1048ae8d71360e1fe6b6601cfb3215c9205
msm-s <ms1756@mailg.denpa.ac.jp>
2020-07-28 14:54:26 +0900

    未完成です sampleのpasswordcheckerにある

--- public/js/password_checker.js
+++ public/js/password_checker.js
@@ -3,30 +3,51 @@
     *8文字以上
     半角英数字のみ
 */
-function getPasswordLevel(password){
+function chkPass(password){
+	var Scorebar = $("scorebar");
     var level = 0;  
     var pattern = 0;  
     var hasLower = false; 
     var hasUpper = false; 
-    var hasNumber = false;  
+	var hasNumber = false;  
+	var AlphaL = "abcdefghijklmnopqrstuvwxyz";
+	var AlphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var Number = "0123456789";
+	var Score;
+	if(password){
+		Score = parseInt(password.length * 8);
+		Length = password.length;
+
+		var arrPassword = password.replace(/\s+/g,"").split(/\s*/);
+		var arrPasswordLen = arrPassword.length;
+
+		for(var cnt=0; cnt<arrPasswordLen; cnt++){
+			if(arrPassword[cnt].match(/[A-Z]/g)){
+				
+			}
+		}
+	}
 
     for (cnt = 0; cnt < password.length; cnt++) {
   
-        var ascii = password.charCodeAt(cnt);
+    	//var ascii = password.charCodeAt(cnt);//UTF-16コードユニット値を表す整数
         
         //アルファベット小文字チェック
-        if ((ascii >= 97) && (ascii <= 122)) {
-          hasLower = true;
+        //if ((ascii >= 97) && (ascii <= 122)) {
+		if(password == AlphaL){
+			hasLower = true;
         }
         
         //アルファベット大文字チェック
-        if ((ascii >= 65) && (ascii <= 90)) {
-          hasUpper = true;
+        //if ((ascii >= 65) && (ascii <= 90)) {
+		if(password == AlphaU){
+			hasUpper = true;
         }
         
         //数値チェック
-        if ((ascii >= 48) && (ascii <= 57)) {
-          hasNumber = true;
+        //if ((ascii >= 48) && (ascii <= 57)) {
+		if(password == Number){
+			hasNumber = true;
         }   
 	}
       
@@ -51,10 +72,7 @@ function getPasswordLevel(password){
     //数値のみパスワードチェック
     if (password.match(/^[0-9]+$/)) {
 		level = 1;
-		return level;
-		console.log(level);
-	}
-	
+	}	
     //長さ：８以上　14未満
     if ((password.length >= 8) && (password.length < 14)) {
 		level = 2;
@@ -67,7 +85,8 @@ function getPasswordLevel(password){
 	if ((password.length >= 8) && (password.length < 14) && (pattern >= 3)) {
 		level = 4;
 	}
-	return level;
+	//return level;
 	
-	$("scorebar").style.backgroundPosition = parseInt(level * 6) + "px";
+	document.getElementsByClassName("scorebar").innerHTML = parseInt(level * 100) + "px";
+//	Scorebar.style.backgroundPosition = "-" + parseInt(level * 4) + "px";
 }
