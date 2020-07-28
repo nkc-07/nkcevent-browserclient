54c06bc2bf502d454b9f6b64d433fc623f44c731
msm-s <ms1756@mailg.denpa.ac.jp>
2020-07-28 12:09:16 +0900

    添削

--- public/js/password_checker.js
+++ public/js/password_checker.js
@@ -3,12 +3,12 @@
     *8文字以上
     半角英数字のみ
 */
-$(function getPasswordLevel(){
+function getPasswordLevel(password){
     var level = 0;  
     var pattern = 0;  
-    var Lower = false; 
-    var Upper = false; 
-    var Number = false;  
+    var hasLower = false; 
+    var hasUpper = false; 
+    var hasNumber = false;  
 
     for (cnt = 0; cnt < password.length; cnt++) {
   
@@ -27,78 +27,47 @@ $(function getPasswordLevel(){
         //数値チェック
         if ((ascii >= 48) && (ascii <= 57)) {
           hasNumber = true;
-        }
-        
-        //記号チェック
-        if (((ascii >= 33) && (ascii <= 47)) ||
-            ((ascii >= 58) && (ascii <= 64)) ||
-            ((ascii >= 91) && (ascii <= 96)) ||
-            ((ascii >= 123) && (ascii <= 126))) {
-            hasCharacter = true;
-        }
-      }
+        }   
+	}
       
-      //パターン判別  
-      if (hasLower) {pattern++;}
-      if (hasUpper) {pattern++;}
-      if (hasNumber) {pattern++;}
-      //if (hasCharacter) {pattern++;}
+    //パターン判別  
+    if (hasLower) {pattern++;}
+    if (hasUpper) {pattern++;}
+    if (hasNumber) {pattern++;}
       
      
-      //パスワードレベル判定 
-    
-      //辞書に登録されている文字チェック
-      var dictionary = ["password","qwerty","abc","admin","root","123"];
-      
-      for (i = 0; i < dictionary.length; i++) {
-        if (password.indexOf(dictionary[i]) != -1) {
-          level = 1;
-          return level;
-        }
-      }
-      
-      //数値のみパスワードチェック
-      if (password.match(/^[0-9]+$/)) {
+	//パスワードレベル判定 
+	  
+    //辞書に登録されている文字チェック
+    var dictionary = ["password","qwerty","abc","admin","root","123",$('input[name=pass]')];
+	  
+    for (i = 0; i < dictionary.length; i++) {
+      if (password.indexOf(dictionary[i]) != -1) {
         level = 1;
         return level;
       }
-    
-      //長さ：８未満
-      if (password.length < 8) {
-        level = 1;
-      }
-      //長さ：８以上　14未満
-      if ((password.length >= 8) && (password.length < 14)) {
-        level = 1;
-      }
-      //長さ：８未満　14未満　パターン２種類以上
-      if ((password.length >= 8) && (password.length < 14) && (pattern >= 2)) {
-        level = 2;
-      }
-      //長さ：８未満　14未満　パターン３種類以上
-      if ((password.length >= 8) && (password.length < 14) && (pattern >= 3)) {
-        level = 3;
-      }
-      //長さ：14以上　パターン３種類未満
-      if ((password.length >= 14) && (pattern < 3)) {
-        level = 2;
-      }
-      //長さ：14以上　パターン３種類以下
-      if ((password.length >= 14) && (pattern >= 3)) {
-        level = 4;
-      }
+    }
       
-      
-      return level;
+    //数値のみパスワードチェック
+    if (password.match(/^[0-9]+$/)) {
+		level = 1;
+		return level;
+		console.log(level);
+	}
+	
+    //長さ：８以上　14未満
+    if ((password.length >= 8) && (password.length < 14)) {
+		level = 2;
+    }
+    //長さ：８未満　14未満　パターン２種類以上
+    if ((password.length >= 8) && (password.length < 14) && (pattern >= 2)) {
+		level = 3;
+    }
+	//長さ：８未満　14未満　パターン３種類以上
+	if ((password.length >= 8) && (password.length < 14) && (pattern >= 3)) {
+		level = 4;
+	}
+	return level;
+	
+	$("scorebar").style.backgroundPosition = parseInt(level * 6) + "px";
 }
-
-//大文字,小文字,数字を含む8文字以上のパスワード
-/*
-$(function(){
-    var reg = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}$/;
-    $('input[name=pass]').match(reg)
-})
-
-*/
-
-
