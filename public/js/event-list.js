01110369250fa5cb40fd5c277b9a645ad79213e0
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-07-31 12:11:59 +0900

    検索情報をDB側に送信する処理を追加

--- public/js/event-list.js
+++ public/js/event-list.js
@@ -1,8 +1,12 @@
+let getRequestParams = (new URL(document.location)).searchParams;
 $(function() {
+    $('[type=serch]').val(getRequestParams.get('event_name'));
+
     let cardDom = $('.card-rink');
 
     $.ajax({
-            url: '../../../api/event/eventlist.php', //送信先
+            url: '/api/event/eventlist.php' +
+                '?event_name=' + getRequestParams.get('event_name'), //送信先
             type: 'GET', //送信方法
             datatype: 'json', //受け取りデータの種類
             data: {}
@@ -13,13 +17,13 @@ $(function() {
             eventCardInfo.forEach(function(cardItem) {
                 cardDom = cardDom.clone();
                 console.log(cardDom);
-                cardDom.attr('href', './detail/index.html?event-id=' + cardItem.event_id);
-                cardDom.find('img').attr('src', '../image/' + cardItem.image);
+                cardDom.attr('href', '/public/html/event-list/detail/index.html?event-id=' + cardItem.event_id);
+                cardDom.find('img').attr('src', '/public/image/' + cardItem.image);
                 cardDom.find('.card-body .held-date').text(cardItem.held_date);
                 cardDom.find('.card-body .event-name').text(cardItem.event_name);
                 cardDom.find('.card-body .map').text(cardItem.map);
                 cardDom.find('.user-info p').text(cardItem.organizer);
-                cardDom.find('.user-info img').attr('src', '../../image/svg/' + cardItem.icon);
+                cardDom.find('.user-info img').attr('src', '/public/image/svg/' + cardItem.icon);
                 cardDom.show();
                 $('.card-columns').append(cardDom);
             })
