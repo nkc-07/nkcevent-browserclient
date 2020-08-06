ac54f699fd721cc5c9a57901bde9168815e76180
masayuki kasano <mk1734@mailg.denpa.ac.jp>
2020-08-06 12:35:03 +0900

    入力判定でのsweetalert2の適応

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,160 +1,190 @@
 /* ユーザ情報を表示するための処理 */
-let sendMemberInfo;
-let myMemberId;
+let sendMemberInfo
+let myMemberId
+var reg = /^[a-z]{2}[0-9]*@mailg.denpa.ac.jp/
 
-$(function() {
-    $.ajax({
-            url: '/api/member/memberinfo.php', //送信先
-            type: 'GET', //送信方法
-            datatype: 'json', //受け取りデータの種類
-            data: {
-                token: localStorage.getItem('token')
-            }
-        })
-        .done(function(response) {
-            let memberInfo = response['data']['info'];
-            let memberTag = response['data']['tag'];
-
-            /* ユーザのステータスを変更するために使用 */
-            sendMemberInfo = memberInfo;
-            myMemberId = memberInfo['member_id']
-            console.log(sendMemberInfo)
-
-            let birtday = memberInfo['birthday'].split('-');
-            $('.user-icon').attr('src', memberInfo['icon']);
-            $('.nickname').val(memberInfo['nickname']);
-            $('.mailaddress').val(memberInfo['mailaddress']);
-            $('.target-year').text(birtday[0]);
-            $('.target-year').val(birtday[0]);
-            $('.target-month').text(birtday[1]);
-            $('.target-month').val(birtday[1]);
-            $('.target-date').text(birtday[2]);
-            $('.target-date').val(birtday[2]);
-            $(`input[value=${memberInfo['gender']}]`).prop('checked', true);
-
-            getParticipationEvent();
-        })
-        .fail(function(response) {
-            console.log('通信失敗');
-            console.log(response);
-        })
-
-    $('.change-button').click(async function(e) {
-        sendMemberInfo['icon'] = await getUserIconName();
-        sendMemberInfo['nickname'] = $('.nickname').val();
-        sendMemberInfo['mailaddress'] = $('.mailaddress').val();
-        sendMemberInfo['birthday'] = $('[id=year]').val() + '-' +
-            $('[id=month]').val() + '-' +
-            $('[id=date]').val()
-        sendMemberInfo['gender'] = $('[name=gender]:checked')[0].value;
-
-        console.log(sendMemberInfo)
-
-        $.ajax({
-            url: '/api/member/memberinfo.php', //送信先
-            type: 'PUT', //送信方法
-            datatype: 'json', //受け取りデータの種類
-            data: sendMemberInfo
-        }).done(function(response) {
-            // location.href = "";
-        }).fail(function(response) {
-            console.log(response);
-        })
-    });
-
-    $('.cancel-button').click(function(e) {
-        location.href = "";
-    })
-});
-
-$('.icon-img, input[type="file"]').on('change', function(e) {
-    let reader = new FileReader();
-    reader.onload = function(e) {
-        $('.list-margin, .user-icon').attr('src', e.target.result);
+$(function () {
+  $.ajax({
+    url: '/api/member/memberinfo.php', //送信先
+    type: 'GET', //送信方法
+    datatype: 'json', //受け取りデータの種類
+    data: {
+      token: localStorage.getItem('token')
     }
-    reader.readAsDataURL(e.target.files[0]);
-});
+  })
+    .done(function (response) {
+      let memberInfo = response['data']['info']
+      let memberTag = response['data']['tag']
 
+      /* ユーザのステータスを変更するために使用 */
+      sendMemberInfo = memberInfo
+      myMemberId = memberInfo['member_id']
+      console.log(sendMemberInfo)
 
-function getParticipationEvent() {
-    /* 参加イベントを表示するための処理 */
-    let eventparticipationDom = $('.participation-event');
+      let birtday = memberInfo['birthday'].split('-')
+      $('.user-icon').attr('src', memberInfo['icon'])
+      $('.nickname').val(memberInfo['nickname'])
+      $('.mailaddress').val(memberInfo['mailaddress'])
+      $('.target-year').text(birtday[0])
+      $('.target-year').val(birtday[0])
+      $('.target-month').text(birtday[1])
+      $('.target-month').val(birtday[1])
+      $('.target-date').text(birtday[2])
+      $('.target-date').val(birtday[2])
+      $(`input[value=${memberInfo['gender']}]`).prop('checked', true)
+
+      getParticipationEvent()
+    })
+    .fail(function (response) {
+      console.log('通信失敗')
+      console.log(response)
+    })
+
+  $('.change-button').click(async function (e) {
+    //入力判定
+    if (
+      $('.nickname').val() != '' &&
+      $('.mailaddress').val() != '' &&
+      reg.test($('.mailaddress').val()) &&
+      $('[id=year]').val() != 0 &&
+      $('[id=month]').val() != 0 &&
+      $('[id=date]').val() != 0
+    ) {
+      sendMemberInfo['icon'] = await getUserIconName()
+      sendMemberInfo['nickname'] = $('.nickname').val()
+      sendMemberInfo['mailaddress'] = $('.mailaddress').val()
+      sendMemberInfo['birthday'] =
+        $('[id=year]').val() +
+        '-' +
+        $('[id=month]').val() +
+        '-' +
+        $('[id=date]').val()
+      sendMemberInfo['gender'] = $('[name=gender]:checked')[0].value
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: '注意',
+        text: '正しく入力してください。'
+      })
+    }
+
+    console.log(sendMemberInfo)
 
     $.ajax({
-            url: '/api/member/memberparticipation.php', //送信先
-            type: 'GET', //送信方法
-            datatype: 'json', //受け取りデータの種類
-            data: {
-                "member_id": myMemberId,
-            }
-        })
-        .done(function(response) {
-            let eventCount = 0;
-            eventdataInfo = response.data;
+      url: '/api/member/memberinfo.php', //送信先
+      type: 'PUT', //送信方法
+      datatype: 'json', //受け取りデータの種類
+      data: sendMemberInfo
+    })
+      .done(function (response) {
+        // location.href = "";
+      })
+      .fail(function (response) {
+        console.log(response)
+      })
+  })
 
-            eventdataInfo.some(function(card) {
-                eventparticipationCloneDom = eventparticipationDom.clone();
-                eventparticipationCloneDom.find("a").attr("href", "/public/html/event-list/detail/index.html?event-id=" + card.event_id);
-                eventparticipationCloneDom.find("a").text(card.event_name);
-                eventparticipationCloneDom.show();
-                $(".participation-event:first").after(eventparticipationCloneDom);
+  $('.cancel-button').click(function (e) {
+    location.href = ''
+  })
+})
 
-                eventCount++;
-                if (eventCount >= 3) {
-                    return true;
-                }
-            });
-            if (eventCount == 0) {
-                eventparticipationDom.show();
-                eventparticipationDom.find('a').remove();
-                eventparticipationDom.find('td').text('参加しているイベントはありません...');
-            }
+$('.icon-img, input[type="file"]').on('change', function (e) {
+  let reader = new FileReader()
+  reader.onload = function (e) {
+    $('.list-margin, .user-icon').attr('src', e.target.result)
+  }
+  reader.readAsDataURL(e.target.files[0])
+})
 
-        })
-        .fail(function(response) {
-            console.log(response);
-        });
+function getParticipationEvent () {
+  /* 参加イベントを表示するための処理 */
+  let eventparticipationDom = $('.participation-event')
+
+  $.ajax({
+    url: '/api/member/memberparticipation.php', //送信先
+    type: 'GET', //送信方法
+    datatype: 'json', //受け取りデータの種類
+    data: {
+      member_id: myMemberId
+    }
+  })
+    .done(function (response) {
+      let eventCount = 0
+      eventdataInfo = response.data
+
+      eventdataInfo.some(function (card) {
+        eventparticipationCloneDom = eventparticipationDom.clone()
+        eventparticipationCloneDom
+          .find('a')
+          .attr(
+            'href',
+            '/public/html/event-list/detail/index.html?event-id=' +
+              card.event_id
+          )
+        eventparticipationCloneDom.find('a').text(card.event_name)
+        eventparticipationCloneDom.show()
+        $('.participation-event:first').after(eventparticipationCloneDom)
+
+        eventCount++
+        if (eventCount >= 3) {
+          return true
+        }
+      })
+      if (eventCount == 0) {
+        eventparticipationDom.show()
+        eventparticipationDom.find('a').remove()
+        eventparticipationDom
+          .find('td')
+          .text('参加しているイベントはありません...')
+      }
+    })
+    .fail(function (response) {
+      console.log(response)
+    })
 }
 
-function getUserIconName() {
-    return new Promise(resolve => {
-        var img = new Image();
-        var reader = new FileReader();
-        var file = $('.send-user-icon').prop('files')[0];
+function getUserIconName () {
+  return new Promise(resolve => {
+    var img = new Image()
+    var reader = new FileReader()
+    var file = $('.send-user-icon').prop('files')[0]
 
-        if ($('.send-user-icon').prop('files')[0] === undefined) {
-            resolve(sendMemberInfo['icon']);
-        }
-        if (!file.type.match(/^image\/(bmp|png|jpeg|gif)$/)) {
-            alert("対応画像ファイル[bmp|png|jpeg|gif]");
-            resolve(sendMemberInfo['icon']);
-        }
+    if ($('.send-user-icon').prop('files')[0] === undefined) {
+      resolve(sendMemberInfo['icon'])
+    }
+    if (!file.type.match(/^image\/(bmp|png|jpeg|gif)$/)) {
+      alert('対応画像ファイル[bmp|png|jpeg|gif]')
+      resolve(sendMemberInfo['icon'])
+    }
 
-        reader.onload = function(event) {
-            img.onload = function() {
-                var data = { data: img.src.split(',')[1] };
-                $.ajax({
-                    url: '/api/event/eventimage.php', //送信先
-                    type: 'POST', //送信方法
-                    data: {
-                        "name": file["name"],
-                        "image": data,
-                    }
-                }).done(function(response) {
-                    console.log('success');
-                    console.log(response);
+    reader.onload = function (event) {
+      img.onload = function () {
+        var data = { data: img.src.split(',')[1] }
+        $.ajax({
+          url: '/api/event/eventimage.php', //送信先
+          type: 'POST', //送信方法
+          data: {
+            name: file['name'],
+            image: data
+          }
+        })
+          .done(function (response) {
+            console.log('success')
+            console.log(response)
 
-                    let imageName = JSON.parse(response);
-                    resolve("/image/" + imageName['data']);
-                }).fail(function(response) {
-                    console.log('通信失敗');
-                    console.log(response);
+            let imageName = JSON.parse(response)
+            resolve('/image/' + imageName['data'])
+          })
+          .fail(function (response) {
+            console.log('通信失敗')
+            console.log(response)
 
-                    resolve(sendMemberInfo['icon']);
-                });
-            };
-            img.src = event.target.result;
-        };
-        reader.readAsDataURL(file);
-    });
+            resolve(sendMemberInfo['icon'])
+          })
+      }
+      img.src = event.target.result
+    }
+    reader.readAsDataURL(file)
+  })
 }
