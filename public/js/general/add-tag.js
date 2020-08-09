d686e35c2b6137be58600db494d47928f05ec421
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-08-09 15:01:01 +0900

    タグ追加画面の大まかなデザインの作成

---
+++ public/js/general/add-tag.js
@@ -0,0 +1,23 @@
+$(function() {
+    let modalDom = $('#modal').clone();
+    $('#modal')[0].remove();
+
+    $('#tag-button').click(function() {
+        Swal.fire({
+            title: 'タグの追加',
+            html: modalDom.show()
+        });
+
+        $('#modal input').on('keydown', function(e) {
+            addTag();
+        });
+        $('#modal .add-tag-button').click(function() {
+            addTag();
+        });
+    })
+});
+
+function addTag() {
+    console.log('push button');
+    //todo..
+}
