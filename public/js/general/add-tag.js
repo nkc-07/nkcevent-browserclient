7d9caa348811865ece08c883923112296db2839c
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-08-10 23:35:51 +0900

    名前保管機能を実装

--- public/js/general/add-tag.js
+++ public/js/general/add-tag.js
@@ -1,4 +1,18 @@
 $(function() {
+    // $.ajax({
+    //     url: '/api/other/tag.php', //送信先
+    //     type: 'GET', //送信方法
+    //     datatype: 'json' //受け取りデータの種類
+    // }).done(function(response) {
+    //     let tagInfo = new Array();
+    //     response['data'].forEach(e => tagInfo.push(e['tag_name']));
+
+
+    // }).fail(function(response) {
+    //     console.log('通信失敗');
+    //     console.log(response);
+    // });
+
     let modalDom = $('#modal').clone();
     $('#modal')[0].remove();
 
@@ -6,6 +20,10 @@ $(function() {
         Swal.fire({
             title: 'タグの追加',
             html: modalDom.show()
+        });
+
+        $('.add-tag .tag-text').MySuggest({
+            msAjaxUrl: '/api/other/tag.php'
         });
 
         $('#modal input').on('keydown', function(e) {
