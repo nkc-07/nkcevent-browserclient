17e9800634b49e70a7787367c140e97975ae7703
Hagiwara0305 <shionn35@gmail.com>
2020-09-26 23:17:04 +0900

    websocketサーバからデータ送られてきたデータを画面に反映

--- public/js/attendance-confirmation.js
+++ public/js/attendance-confirmation.js
@@ -1,5 +1,30 @@
 let getRequestParams = (new URL(document.location)).searchParams;
 let eventAttendanceId;
+let attendanceIcons = {
+    0: {
+        img: '/public/image/svg/question.svg',
+        text: '未確認'
+    },
+    1: {
+        img: '/public/image/svg/cross.svg',
+        text: '欠席'
+    },
+    2: {
+        img: '/public/image/svg/check.svg',
+        text: '出席確認済み'
+    }
+};
+
+var conn = new WebSocket('ws://localhost:81?mode=attendance&participation_event=' + getRequestParams.get('event-id'));
+conn.onopen = function(e) {
+    console.log("Connection established!");
+};
+
+conn.onmessage = function(e) {
+    console.log(e.data);
+    $('.member-id-' + e.data + ' .svg').attr('src', attendanceIcons[2].img);
+    $('.member-id-' + e.data + ' .dropdown-toggle').text(attendanceIcons[2].text);
+};
 
 $(document).on('click', '.dropdown-menu .dropdown-item', function() {
     var visibleItem = $('.dropdown-toggle', $(this).closest('.dropdown'));
@@ -42,29 +67,18 @@ $.ajax({
         }
     })
     .done(function(response) {
+        console.log(response)
         if (!response['data']['info']['member_id'] == eventAttendanceId) {
             location.href = '/public/html/event-list/detail/?event-id=' + getRequestParams.get('event-id');
+        } else {
+            $('.organizer-box img').attr('src', response['data']['info']['icon']);
+            $('.organizer-box p').text(response['data']['info']['nickname']);
         }
     })
     .fail(function(response) {
         console.log('通信失敗');
         console.log(response);
     })
-
-let attendanceIcons = {
-    0: {
-        img: '/public/image/svg/question.svg',
-        text: '未確認'
-    },
-    1: {
-        img: '/public/image/svg/cross.svg',
-        text: '欠席'
-    },
-    2: {
-        img: '/public/image/svg/check.svg',
-        text: '出席確認済み'
-    }
-};
 
 $.ajax({
         url: '/api/event/eventparticipant.php', //送信先
@@ -79,6 +93,7 @@ $.ajax({
         response.data.forEach(function(items) {
             console.log(items)
             attendanceUserDom = attendanceUserDom.clone();
+            attendanceUserDom.addClass('member-id-' + items.member_id);
             attendanceUserDom.find('.user-icon img').attr('src', items.icon);
             attendanceUserDom.find('.svg').attr('src', attendanceIcons[items.is_attendance].img);
             attendanceUserDom.find('.dropdown-toggle').text(attendanceIcons[items.is_attendance].text);
