5f71141d17f3660ddfd8f1f744f344e1315710b3
Hagiwara0305 <shionn35@gmail.com>
2020-10-11 13:39:08 +0900

    apiからデータを持ってくる処理を追加

--- public/js/general/pager.js
+++ public/js/general/pager.js
@@ -1,52 +1,61 @@
 $(function() {
-    data = { // TODO: ajaxにて最大数を持ってくる処理を追加
-        'max': 10
-    };
+    $.ajax({
+            url: '/api/event/eventpager.php', //送信先
+            type: 'GET', //送信方法
+            datatype: 'json', //受け取りデータの種類
+            data: { limit: 30 }
+        }).done(function(response) {
+            let now = getRequestParams.get('page') == null ? 1 : getRequestParams.get('page');
+            now = Number(now);
 
-    let now = getRequestParams.get('page') == null ? 1 : getRequestParams.get('page');
-    now = Number(now);
+            generateGetUrl(now - 1, $('.pagination .back'));
+            generateGetUrl(now + 1, $('.pagination .next'));
+            let pagerItem = $('.pagination .page-item.list-item').clone();
 
-    generateGetUrl(now - 1, $('.pagination .back'));
-    generateGetUrl(now + 1, $('.pagination .next'));
-    if (now == 1) {
-        $('.pagination .back').addClass('disabled');
-    }
+            let pageCnt = Math.abs(
+                now > 2 ?
+                (now <= response.data.page_cunt - 2 ?
+                    now - 1 : response.data.page_cunt - 2
+                ) : 1
+            );
+            if (pageCnt < response.data.page_cunt) {
+                if (now != 1) {
+                    $('.pagination .back').removeClass('disabled');
+                }
 
-    let pagerItem = $('.pagination .page-item.list-item').clone();
+                if (now > 2) {
+                    addPagerItem(pagerItem.clone(), 1, false);
+                    if (now != 3) {
+                        addPagerItem(pagerItem.clone(), '...', true);
+                    }
+                }
 
-    if (now > 2) {
-        addPagerItem(pagerItem.clone(), 1, false);
-        if (now != 3) {
-            addPagerItem(pagerItem.clone(), '...', true);
-        }
-    }
+                for (let i = 0; i < 3; i++) {
+                    let currentDom = addPagerItem(pagerItem.clone(), pageCnt, false);
 
-    let pageCnt = (
-        now > 2 ?
-        (now <= data['max'] - 2 ?
-            now - 1 : data['max'] - 2
-        ) : 1
-    );
-    for (let i = 0; i < 3; i++) {
-        let currentDom = addPagerItem(pagerItem.clone(), pageCnt, false);
+                    if (now == pageCnt) {
+                        currentDom.addClass('active');
+                    }
 
-        if (now == pageCnt) {
-            currentDom.addClass('active');
-        }
+                    pageCnt++;
+                }
 
-        pageCnt++;
-    }
+                if (now <= response.data.page_cunt - 2) {
+                    if (now != response.data.page_cunt - 2) {
+                        addPagerItem(pagerItem.clone(), '...', true);
+                    }
+                    addPagerItem(pagerItem.clone(), response.data.page_cunt, false);
+                }
 
-    if (now <= data['max'] - 2) {
-        if (now != data['max'] - 2) {
-            addPagerItem(pagerItem.clone(), '...', true);
-        }
-        addPagerItem(pagerItem.clone(), data['max'], false);
-    }
-
-    if (now == data['max']) {
-        $('.pagination .next').addClass('disabled');
-    }
+                if (now != response.data.page_cunt) {
+                    $('.pagination .next').removeClass('disabled');
+                }
+            }
+        })
+        .fail(function(response) {
+            console.log('通信失敗');
+            console.log(response);
+        })
 })
 
 function addPagerItem(dom, insertValue, is_disabled) {
