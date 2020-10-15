0c820e03b737391412473d0e54b4a413ff2ed947
Hagiwara0305 <shionn35@gmail.com>
2020-10-15 23:30:11 +0900

    1000文字に制限

--- public/js/new.js
+++ public/js/new.js
@@ -26,6 +26,16 @@ var simplemde = new SimpleMDE({
     element: $("#markdown-editor")[0]
 });
 
+simplemde.codemirror.on('beforeChange', (instance, changes) => {
+    if (simplemde.value().length >= 1000 &&
+        changes.origin !== "+delete" &&
+        changes.origin !== "setValue" // value()で再帰禁止用
+    ) {
+        changes.cancel();
+        simplemde.value(simplemde.value().slice(0, 1000)); // コピペの場合、変化が見えないので1000文字文のみ前から置き換える
+    }
+});
+
 /* イベントを作成するために使用する各情報を入れておくための変数 */
 let createEventInfo = {
     'event_name': undefined,
