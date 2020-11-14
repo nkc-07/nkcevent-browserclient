c88f2bcefe31010581997aa70bc9584dc332a132
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-11-14 16:46:24 +0900

    chatページに参加グループ一覧を追加

--- public/js/group-chat.js
+++ public/js/group-chat.js
@@ -27,6 +27,9 @@ $.ajax({
 })
 
 $(function() {
+    groupparticipatedList();
+    groupappryList();
+
     $.ajax({
         url: '/api/group/groupchat.php', //送信先
         type: 'GET', //送信方法
@@ -98,3 +101,76 @@ function addMessage(element) {
     );
     bottomScroll();
 }
+
+function groupparticipatedList() {
+    var group_searchtype = 2;
+    var group_pram = localStorage.getItem('token') ? localStorage.getItem('token') : 0;
+    console.log(group_pram);
+    $.ajax({ 
+        url: '/api/group/grouplist.php', //送信先
+        type: 'GET', //送信方法
+        datatype: 'json', //受け取りデータの種類
+        data: {
+            'group_searchtype': group_searchtype,
+            'group_pram': group_pram,
+        }
+    })
+    .done(function(response) {
+        let groupparticipatedDom = $('.group-participated');
+        grouplist = response.data;
+        if (grouplist.length > 0) {
+            grouplist.forEach(function(groupparticipatedInfo) {
+                groupparticipatedDom = groupparticipatedDom.clone();
+                //console.log(groupparticipatedInfo);
+                groupparticipatedDom.find('a').attr('href', '/public/html/group/group-detail/chat/index.html?group-id='+groupparticipatedInfo.group_id);
+                groupparticipatedDom.find('a').html(groupparticipatedInfo.group_name);
+                groupparticipatedDom.show();
+                $(".group-participated-list").append(groupparticipatedDom);
+            })
+        }
+    })
+    .fail(function(response) {
+        console.log('通信失敗');
+        console.log(response);
+        //location.href = '/public/html/event-list/';
+    })
+}
+function groupappryList() {
+    var group_searchtype = 3;
+    var group_pram = localStorage.getItem('token') ? localStorage.getItem('token') : 0;
+    console.log(group_pram);
+    $.ajax({ 
+        url: '/api/group/grouplist.php', //送信先
+        type: 'GET', //送信方法
+        datatype: 'json', //受け取りデータの種類
+        data: {
+            'group_searchtype': group_searchtype,
+            'group_pram': group_pram,
+        }
+    })
+    .done(function(response) {
+        let groupappryDom = $('.group-appry');
+        grouplist = response.data;
+        if (grouplist.length > 0) {
+            grouplist.forEach(function(groupappryInfo) {
+                groupappryDom = groupappryDom.clone();
+                //console.log(groupappryInfo);
+                groupappryDom.find('a').attr('href', '/public/html/group/group-detail/index.html?group-id='+groupappryInfo.group_id);
+                groupappryDom.find('a').html(groupappryInfo.group_name);
+                groupappryDom.show();
+                $(".group-appry-list").append(groupappryDom);
+            })
+        }
+    })
+    .fail(function(response) {
+        console.log('通信失敗');
+        console.log(response);
+        //location.href = '/public/html/event-list/';
+    })
+}
+$('.serch-button').on('click', serchGroupList);
+$('.serch-input').on('keyup', function (e) {
+    if (e.keyCode == '13') {
+        serchGroupList();
+    }
+})
